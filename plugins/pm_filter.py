e1e9b0f78ab2cecc019b53c2da5f3ad4cf90cad0
KO_Paing_Lay <89724508+sittpaing123@users.noreply.github.com>
2022-12-08 16:44:37 +0630

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -859,7 +859,7 @@ async def advantage_spell_chok(msg):
 async def manual_filters(client, message, text=False):
     group_id = message.chat.id
     name = text or message.text
-    reply_id = message.reply_to_message.from_user.id if message.reply_to_message else message.message_id
+    reply_id = message.reply_to_message.from_user.id if message.reply_to_message else message.from_user.id
     keywords = await get_filters(group_id)
     for keyword in reversed(sorted(keywords, key=len)):
         pattern = r"( |^|[^\w])" + re.escape(keyword) + r"( |$|[^\w])"
