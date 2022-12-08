82d82fd625b948062da1e1a21d8b9dfdab64db1e
KO_Paing_Lay <89724508+sittpaing123@users.noreply.github.com>
2022-12-08 17:22:04 +0630

    Update info.py

--- info.py
+++ info.py
@@ -65,7 +65,7 @@ FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).
 MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "False")), True)
 PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "False")), False)
 PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "False")), True)
-
+G_FILTER = bool(environ.get("G_FILTER", True))
 
 
 LOG_STR = "Current Cusomized Configurations are:-\n"
