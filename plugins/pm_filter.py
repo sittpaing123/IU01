5773f13ac2c7d42aab9f7a193928c59f0eca913f
KO_Paing_Lay <89724508+sittpaing123@users.noreply.github.com>
2022-12-08 17:25:43 +0630

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -17,6 +17,7 @@ from database.gfilters_mdb import find_gfilter, get_gfilters
 from pyrogram.errors import FloodWait, UserIsBlocked, MessageNotModified, PeerIdInvalid
 from utils import get_size, is_subscribed, get_poster, search_gagala, temp, get_settings, save_group_settings
 from database.users_chats_db import db
+from plugins.helper.admin_check import admin_fliter
 from database.ia_filterdb import Media, get_file_details, get_search_results
 from database.filters_mdb import (
     del_all,
